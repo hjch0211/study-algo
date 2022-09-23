461901c4dca13aab703805890006bdb303b197b1
Jchan <gkswocks123@gmail.com>
2022-09-23 14:08:12 +0900

    Feat : 접근 자체가 잘못된 듯. 96점

--- week2/p2.js
+++ week2/p2.js
@@ -1,5 +1,5 @@
 /**
- * 조이스틱 [40분, 실패] : 왜 틀린지 모르겠음
+ * 조이스틱 [40분, 96점 실패] : 처음갔다가 돌아오는 케이스 못품
  * @param {} name
  * @returns
  */
@@ -8,7 +8,7 @@ function solution(name) {
     var answer = 0; //최소 조작값 리턴
 
     const slicedName = sliceName(name);
-    console.log(slicedName);
+
     const up = ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
     const down = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O"];
 
@@ -19,9 +19,7 @@ function solution(name) {
         else answer += down.indexOf(slicedName[i]) + 1;
     }
 
-    // 이동을 안하므로
-    if (slicedName === name && name.includes("A")) return answer;
-    else return answer + slicedName.length - 1;
+    return answer + minMove(slicedName, name);
 }
 
 // JAASSAAA 첫째자리 기준 연속된 A가 적은 곳으로 이동해야 함
@@ -47,8 +45,51 @@ const sliceName = (name) => {
     return slicedName || name;
 };
 
+const minMove = (slicedName, name) => {
+    let mostLongAIndex = 0;
+    let mostLongALength = 0;
+    let tempIndex = 0;
+    let tempLength = 0;
+
+    // 중간에 긴 연속된 A가 있을 때
+    // ABBBBAAAAABB : B만큼 갔다가 B만큼 돌아오고 뒤에서부터읽음
+    //
+    // B*2 + A*5  // 2*(B -B)
+
+    // 문자열에서 가장 긴 연속된 A의 인덱스 위치와 길이를 구해야 됨.
+    for (let i = 1; i < name.length; i++) {
+        if (name[i] === "A" && tempLength === 0) {
+            tempIndex = i;
+            tempLength++;
+        } else if (name[i - 1] == "A" && name[i] === "A" && tempLength > 0) {
+            tempLength++;
+        } else {
+            tempIndex = 0;
+            tempLength = 0;
+        }
+
+        if (tempLength > mostLongALength) {
+            mostLongAIndex = tempIndex;
+            mostLongALength = tempLength;
+        }
+    }
+
+    // 이동을 안하므로
+    if (slicedName === name && !slicedName.match(/[B-Z]/)) return 0;
+    else if (mostLongALength === 0) return slicedName.length - 1;
+    else
+        return Math.min(
+            name.length - 1,
+            2 * (mostLongAIndex - 1) + (name.length - (mostLongAIndex + mostLongALength)),
+            mostLongAIndex - 1 + 2 * (name.length - (mostLongAIndex + mostLongALength))
+        );
+};
+
 module.exports = function p2() {
     console.log(solution("JEROEN"));
     console.log(solution("JAN"));
     console.log(solution("BAAAAASAABAA"));
+    console.log(solution("BBAAAAAAASAABB")); // ...
+    console.log(solution("ABAAAAAB"));
+    console.log(solution("AABAA"));
 };
---
+++ week2/p3.js
@@ -0,0 +1,6 @@
+module.exports = function p3() {
+    console.log(solution("JEROEN"));
+    console.log(solution("JAN"));
+    console.log(solution("BAAAAASAABAA"));
+    console.log(solution("BBAAAAAASAABB")); // ...
+};
