b9b3e477f1b52190e6e8308a8813c45b3189b376
Jchan <gkswocks123@gmail.com>
2022-10-15 00:15:34 +0900

    p3 푸는중

--- week5/index.js
+++ week5/index.js
@@ -1,7 +1,7 @@
 // const p1 = require("./p1");
-const p2 = require("./p2");
-// const p3 = require("./p3");
+// const p2 = require("./p2");
+const p3 = require("./p3");
 
 // p1();
-p2();
-// p3();
+// p2();
+p3();
---
+++ week5/p3/index.js
@@ -0,0 +1,9 @@
+function solution(game_board, table) {
+    var answer = -1;
+    // 모든 퍼즐 조각은 격자 칸에 딱 맞게 놓여있음 -> 하나의 빈칸에 두개가 들어가지는 못함
+    return answer;
+}
+
+module.exports = function p3() {
+    console.log(solution());
+};
